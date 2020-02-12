7acac56337ce7cab0ce990f34366060d96967715
HANYONG LEE <glhy0718@gmail.com>
2020-02-12 14:18:46 -0800

    final

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, make_response, jsonify, Response
 from cam import VideoCamera
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -18,10 +20,26 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+def gen_snapshot(camera):
+    frame = camera.get_frame()
+    yield (frame)
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/snapshot.jpg')
+def snapshot():
+    return Response(gen_snapshot(VideoCamera()),
+                    mimetype='image/jpg')
+
+# example for ajax
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True)
+    app.run(debug=True, host='0.0.0.0', threaded=True, port=8080)
